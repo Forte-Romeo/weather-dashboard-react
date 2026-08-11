fdc2d1740ee917fad634b22cfa75a9bb7a06f2d3
Forte-Romeo <fergusonromeonyarmah@gmail.com>
2026-08-11 14:25:20 +0000

    update: static hourly forecast info

--- src/components/HourlyForecast.jsx
+++ src/components/HourlyForecast.jsx
@@ -1,7 +1,62 @@
 function HourlyForecast() {
+
+    const forecast = [
+        {
+            time: "6:00 AM",
+            temperature: "25°",
+            icon: "☁️"
+        },
+        {
+            time: "9:00 AM",
+            temperature: "28°",
+            icon: "🌤️"
+        },
+        {
+            time: "12:00 PM",
+            temperature: "31°",
+            icon: "☀️"
+        },
+        {
+            time: "3:00 PM",
+            temperature: "33°",
+            icon: "☀️"
+        },
+        {
+            time: "6:00 PM",
+            temperature: "30°",
+            icon: "🌤️"
+        },
+        {
+            time: "9:00 PM",
+            temperature: "27°",
+            icon: "☁️"
+        },
+    ];
+
     return (
-        <section>
-            <h2>Hourly Forecast</h2>
+        <section className="forecast-card">
+
+            <h3>TODAY'S FORECAST</h3>
+
+            <div className="hourly-forecast">
+
+                {forecast.map((hour) => (
+                    <div 
+                        className="hour-card"
+                        key={hour.time}
+                    >
+                        
+                        <p>{hour.time}</p>
+                        <span className="forecast-icon">
+                            {hour.icon}
+                        </span>
+                        <h4>{hour.temperature}</h4>
+
+                    </div>
+                ))}
+                
+            </div>
+
         </section>
     );
 }
