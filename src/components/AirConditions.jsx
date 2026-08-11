9a2f2013fc74a42f1d48031c21ccbef94778e365
Forte-Romeo <fergusonromeonyarmah@gmail.com>
2026-08-11 14:35:45 +0000

    update: static Air Conditions info

--- src/components/AirConditions.jsx
+++ src/components/AirConditions.jsx
@@ -1,7 +1,62 @@
 function AirConditions() {
+
+    const conditions = [
+        {
+            icon: "🌡️",
+            label: "Real Feel",
+            value: "32°"
+        },
+        {
+            icon: "💨",
+            label: "Wind",
+            value: "4.2 km/h"
+        },
+        {
+            icon: "💧",
+            label: "Humidity",
+            value: "78%"
+        },
+        {
+            icon: "☀️",
+            label: "UV Index",
+            value: "6"
+        }
+    ];
+
     return (
-        <section>
-            <h2>Air Conditions</h2>
+        <section className="air-conditions">
+            <div className="section-header">
+                <h3>AIR CONDITIONS</h3>
+                <button className="see-more-btn">
+                    See More
+                </button>
+            </div>
+            
+            <div className="conditions-grid">
+                {conditions.map((condition) =>(
+                    <div 
+                        className="condition-card"
+                        key={condition.label}
+                    >
+
+                        <div className="condition-title">
+                            <span>
+                                {condition.icon}
+                            </span>
+
+                            <span>
+                                {condition.label}
+                            </span>
+                        </div>
+
+                        <h4>
+                            {condition.value}
+                        </h4>
+
+                    </div>
+                ))}
+            </div>
+            
         </section>
     );
 }
