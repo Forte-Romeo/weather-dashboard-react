72209dbca37859707cdb540773daaf1a756d906a
Forte-Romeo <fergusonromeonyarmah@gmail.com>
2026-08-11 14:14:21 +0000

    update: static current weather info

--- src/components/CurrentWeather.jsx
+++ src/components/CurrentWeather.jsx
@@ -1,7 +1,22 @@
 function CurrentWeather() {
     return (
-        <section>
-            <h2>Current Weather</h2>
+        <section className="current-weather">
+            
+            <div className="weather-info">
+                <h1>Accra</h1>
+                <p className="rain-chance">
+                    Chance of rain: 20%
+                </p>
+                <h2>31°</h2>
+            </div>
+
+            <div className="weather-icon">
+                <img 
+                    src="https://openweathermap.org/img/wn/01d@4x.png" 
+                    alt="Sunny Weather" 
+                />
+            </div>
+            
         </section>
     );
 }
