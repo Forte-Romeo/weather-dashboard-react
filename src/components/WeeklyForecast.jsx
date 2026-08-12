1903075158bdacb7f570878975cb9dfdd3d3e4c1
Forte-Romeo <fergusonromeonyarmah@gmail.com>
2026-08-12 15:10:47 +0000

    update: static weekly forecast info

--- src/components/WeeklyForecast.jsx
+++ src/components/WeeklyForecast.jsx
@@ -1,8 +1,92 @@
 function WeeklyForecast() {
+
+    const forecast = [
+        {
+            day: "Today",
+            condition: "Sunny",
+            icon: "☀️",
+            high: "36°",
+            low: "22°"
+        },
+        {
+            day: "Tue",
+            condition: "Sunny",
+            icon: "☀️",
+            high: "37°",
+            low: "21°"
+        },
+        {
+            day: "Wed",
+            condition: "Sunny",
+            icon: "☀️",
+            high: "37°",
+            low: "21°"
+        },
+        {
+            day: "Thu",
+            condition: "Cloudy",
+            icon: "☁️",
+            high: "35°",
+            low: "22°"
+        },
+        {
+            day: "Fri",
+            condition: "Cloudy",
+            icon: "☁️",
+            high: "34°",
+            low: "22°"
+        },
+        {
+            day: "Sat",
+            condition: "Rainy",
+            icon: "🌧️",
+            high: "32°",
+            low: "21°"
+        },
+        {
+            day: "Sun",
+            condition: "Storm",
+            icon: "⛈️",
+            high: "31°",
+            low: "20°"
+        }
+    ];
+
     return (
-        <section>
-            <h2>7-Day Forecast</h2>
-        </section>
+        <aside className="weekly-forecast">
+
+            <h3>7-DAY FORECAST</h3>
+
+            <div className="weekly-list">
+
+                {forecast.map((day) => (
+                    <div
+                        className="day-card"
+                        key={day.day}
+                    >
+
+                        <p>{day.day}</p>
+
+                        <div className="day-weather">
+                            <span className="day-icon">
+                                {day.icon}
+                            </span>
+
+                            <span>
+                                {day.condition}
+                            </span>
+                        </div>
+
+                        <p>
+                            {day.high} / {day.low}
+                        </p>
+
+                    </div>
+                ))}
+
+            </div>
+
+        </aside>
     );
 }
 
