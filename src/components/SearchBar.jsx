b2490aad5376ff1d3af5cc1299c418a783eda8ce
Forte-Romeo <fergusonromeonyarmah@gmail.com>
2026-08-13 11:38:30 +0000

    update: search state

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,27 @@
+import { useState } from "react";
+
 function SearchBar () {
+
+    const [searchTerm, setSearchTerm] = useState("");
+
+    function handleInputChange(event) {
+        setSearchTerm(event.target.value);
+    }
+
+    function handleSearch() {
+        console.log("Searching for:", searchTerm);
+    }
+
     return (
         <div className="search-container">
             <input 
                 type="text" 
                 placeholder="Search for cities"
+                value={searchTerm}
+                onChange={handleInputChange}
             />
 
-            <button>
+            <button onClick={handleSearch}>
                 🔍
             </button>
         </div>
