fa6dea982bfee07e809773dc4d295b329efc5bde
Forte-Romeo <fergusonromeonyarmah@gmail.com>
2026-08-16 14:11:30 +0000

    update: Current Weather UI refactored

--- src/components/CurrentWeather.jsx
+++ src/components/CurrentWeather.jsx
@@ -2,21 +2,25 @@ function CurrentWeather({ weather }) {
 
     const { location, current } = weather;
 
+    const temperature = Math.round(current.temp_c);
+
+    const condition = current.condition.text;
+
     return (
         <section className="current-weather">
             
             <div className="weather-info">
                 <h1>{location.name}</h1>
                 <p className="rain-chance">
-                    Chance of rain: {current.condition.text}
+                    Condition: {condition}
                 </p>
-                <h2>{Math.round(current.temp_c)}°</h2>
+                <h2>{temperature}°</h2>
             </div>
 
             <div className="weather-icon">
                 <img 
                     src={`https:${current.condition.icon}`}
-                    alt={current.condition.text} 
+                    alt={condition} 
                 />
             </div>
             
